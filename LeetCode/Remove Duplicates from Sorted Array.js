d966f042875d27618ed16117aea422e7f417403b
unknown <aditya.india.dev@gmail.com>
2024-06-16 15:50:50 +0530

    first commit

--- LeetCode/Remove Duplicates from Sorted Array.js
+++ LeetCode/Remove Duplicates from Sorted Array.js
@@ -1,33 +1,64 @@
 // 26. Remove Duplicates from Sorted Array
 
-let nums =  [-1,0,0,0,0,3,3]
+let nums = [-1, 0, 0, 0, 0, 3, 3]
 
-var removeDuplicates = function (nums) {
+// var removeDuplicates = function (nums) {
 
-    let uniqueElemSet = new Set()
-    for(i = 0; i<nums.length; i++)
-    {
-        const elem = nums[i]
-        uniqueElemSet.add(elem)
+//     let uniqueElemSet = new Set()
+//     for(i = 0; i<nums.length; i++)
+//     {
+//         const elem = nums[i]
+//         uniqueElemSet.add(elem)
+//     }
+
+//     let uniqueElemArr = [...uniqueElemSet]
+
+//     const k = uniqueElemArr.length
+
+//     for(i=0; i<(nums.length-k); i++){
+//         uniqueElemArr.push("_")
+//     }
+
+//     nums.length = 0
+
+//     uniqueElemArr.forEach((item)=>{
+//         nums.push(item)
+//     })
+
+//     return k
+
+// };
+
+var removeDuplicates = function (nums = []) {
+
+    let uniqueElemObj = undefined
+
+    let lastUniqueElemPosition = -1
+
+    for (let index = 0; index < nums.length; index++) {
+        const element = nums[index];
+
+        if (uniqueElemObj == element) {
+        }
+        else {
+    
+            lastUniqueElemPosition++
+            nums[lastUniqueElemPosition] = element
+            uniqueElemObj = element
+        }
+
     }
 
-    let uniqueElemArr = [...uniqueElemSet]
 
-    const k = uniqueElemArr.length
 
-    for(i=0; i<(nums.length-k); i++){
-        uniqueElemArr.push("_")
+    for (let index = (lastUniqueElemPosition + 1); index < (nums.length); index++) {
+        nums[index] = "_"
+
     }
+    console.log(uniqueElemObj);
+    return (lastUniqueElemPosition + 1)
 
-    nums.length = 0
-
-    uniqueElemArr.forEach((item)=>{
-        nums.push(item)
-    })
-
-    return k
-
-};
+}
 
 console.log(removeDuplicates(nums));
 console.log(nums);
